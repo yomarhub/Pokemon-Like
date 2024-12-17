63c4ac6e2884ec3dfe9a12fc3829612dd244d1ee
yomarhub <omar.yassine@ynov.com>
2024-12-17 22:38:30 +0100

    add monsters

--- PokeLike/MVVM/ViewModel/GameViewVM.cs
+++ PokeLike/MVVM/ViewModel/GameViewVM.cs
@@ -1,11 +1,16 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Media;
+using Microsoft.EntityFrameworkCore;
+using PokeLike.Functions;
+using PokeLike.Model;
 
 namespace PokeLike.MVVM.ViewModel
 {
     public class GameViewVM : BaseVM
     {
         #region Variables
+        public static Action<Spell> Attack { get; set; }
         private readonly Dictionary<ImageBrush, string> _backgrounds = [];
         private ImageBrush? currentBack;
         public ImageBrush? CurrentBack
@@ -25,6 +30,13 @@ namespace PokeLike.MVVM.ViewModel
 
         private bool isFighting = true;
         public bool IsFighting { get => isFighting; set { SetProperty(ref isFighting, value); OnPropertyChanged(nameof(IsFighting)); } }
+
+        private ObservableCollection<Monster> AllMonsters;
+        private BattleMonster ally;
+        public BattleMonster Ally { get => ally; set { SetProperty(ref ally, value); OnPropertyChanged(nameof(Ally)); } }
+
+        private BattleMonster ennemy;
+        public BattleMonster Ennemy { get => ennemy; set { SetProperty(ref ennemy, value); OnPropertyChanged(nameof(Ennemy)); } }
         #endregion
         public GameViewVM() : base()
         {
@@ -34,20 +46,34 @@ namespace PokeLike.MVVM.ViewModel
                     _backgrounds?.Add(found, b);
                 });
             ChangeBack();
+            Attack = AttackEnnemy;
+            AllMonsters = new(_context.Monsters.Include(m => m.Spells));
+            if (Session.User != null && Session.CurrentMonster != null) Ally = new(Session.CurrentMonster);
+            else Ally = new(AllMonsters.First()) { Name = "No pokemon has been choosen" };
+        }
+        public void AttackEnnemy(Spell s)
+        {
+            s.Attack(Ennemy);
+        }
+        public void GetNewEnnemy()
+        {
+            Random rand = new();
+            Ennemy = new(AllMonsters.ElementAt(rand.Next(AllMonsters.Count))) { OnDamage = HandleOnDamage };
+        }
+        public void HandleOnDamage(int health)
+        {
+            if (health <= 0)
+            {
+                Ennemy.OnDamage -= HandleOnDamage;
+                GetNewEnnemy();
+                return;
+            }
         }
         private void ChangeBack()
         {
             if (_backgrounds.Count < 1) return;
             Random rand = new();
             CurrentBack = _backgrounds.ElementAt(rand.Next(_backgrounds.Count)).Key;
-        }
-        public override void OnShowView()
-        {
-            base.OnShowView();
-        }
-        public override void OnHideView()
-        {
-            base.OnHideView();
         }
     }
 }
