2a57303dabd826c1ad15dfbd8271a01f9691e8ea
yomarhub <omar.yassine@ynov.com>
2024-12-17 16:18:34 +0100

    Remove LoginId

--- PokeLike/MVVM/ViewModel/CreatePlayerVM.cs
+++ PokeLike/MVVM/ViewModel/CreatePlayerVM.cs
@@ -9,7 +9,6 @@ namespace PokeLike.MVVM.ViewModel
     public class CreatePlayerVM : INotifyPropertyChanged
     {
         private string? _name;
-        private int? _loginId;
         private string? _errorText;
         public string? ErrorText { get => _errorText; set { _errorText = value; OnPropertyChanged(nameof(ErrorText)); } }
 
@@ -20,49 +19,31 @@ namespace PokeLike.MVVM.ViewModel
         }
 
         public Login Login { get; private set; }
-
-        public int? LoginId
-        {
-            get => _loginId;
-            set { _loginId = value; OnPropertyChanged(nameof(LoginId)); }
-        }
-
         public ICommand SaveCommand { get; }
         public ICommand CancelCommand { get; }
 
         public Action? OnDialogClose { get; set; }
         public Player? CreatedPlayer { get; private set; }
 
-        public CreatePlayerVM()
+        public CreatePlayerVM(Login l)
         {
             SaveCommand = new RelayCommand(Save);
             CancelCommand = new RelayCommand(Cancel);
-        }
-        public CreatePlayerVM(Login l) : this()
-        {
             Login = l;
-            LoginId = (l.Id > 0) ? l.Id : 1;
         }
 
         private ExerciceMonsterContext context = new();
         private void Save()
         {
-            Login? last = context.Logins.Order().LastOrDefault();
             if (string.IsNullOrWhiteSpace(this.Name))
             {
                 ErrorText = "Name is required";
                 return;
             }
-            else if (this.LoginId < 1)
-            {
-                ErrorText = "LoginId is required";
-                return;
-            }
-            else if (last == null || LoginId > last.Id) { ErrorText = "invalid LoginId"; return; }
             CreatedPlayer = new Player
             {
                 Name = this.Name!,
-                LoginId = this.LoginId
+                LoginId = this.Login.Id
             };
 
             OnDialogClose?.Invoke();
@@ -80,7 +61,7 @@ namespace PokeLike.MVVM.ViewModel
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
         {
             if (!Equals(field, newValue))
             {
