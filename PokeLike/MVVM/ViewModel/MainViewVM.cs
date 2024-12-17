08d3b5747203ad6c77a652d69bb1c7bf72c3fbf7
yomarhub <omar.yassine@ynov.com>
2024-12-17 16:13:37 +0100

    Commented Flipper

--- PokeLike/MVVM/ViewModel/MainViewVM.cs
+++ PokeLike/MVVM/ViewModel/MainViewVM.cs
@@ -7,7 +7,7 @@ namespace PokeLike.MVVM.ViewModel
         #region Variables
         public RelayCommand RequestDB { get; set; }
         // true = is not animated / false = is animated / null = default
-        public static bool SpellAnim { get; set; } = true;
+        //public static bool SpellAnim { get; set; } = true;
         #endregion
         public MainViewVM() : base()
         {
